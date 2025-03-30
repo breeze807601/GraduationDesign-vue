b65e6238d269b3b44695c174122afedd074826b3
breeze807601 <2072063341@qq.com>
2025-03-30 20:22:33 +0800

    日期组件修改

--- src/components/CalendarComponent.vue
+++ src/components/CalendarComponent.vue
@@ -31,68 +31,12 @@
 <script setup>
 import calendar from "@/utils/calendar";
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
-import {onMounted, ref} from "vue";
-import {ElNotification} from "element-plus";
-
-onMounted(() => {
-    workNotice();
-})
 
 const myCalendar = ref();
 const selectDate = (val) => {
     if (!myCalendar.value) return
     myCalendar.value.selectDate(val)
 }
-
-// 通知记录抄表和录入抄表的信息提示
-function workNotice() {
-    // 获取年月
-    let date = new Date();
-    let year = date.getFullYear();
-    let month = date.getMonth();
-    // 获取下个月的第一天
-    const firstDayNextMonth = new Date(year, month + 1, 1);
-    // 获取下个月第一天的前一天（即本月最后一天）
-    const lastDayThisMonth = new Date(firstDayNextMonth - 1);
-    // 获取本月倒数第二天
-    // firstDayNextMonth -  86400000 = x月最后一天的00：00：00
-    // firstDayNextMonth -  86400001 = x月倒数第二天的23：59：59
-    const secondLastDayThisMonth = new Date(firstDayNextMonth -  86400001);
-
-    // 格式化日期为 YYYY-MM-DD
-    let lastDayFormatted = formatDate(lastDayThisMonth);
-    let secondLastDayFormatted = formatDate(secondLastDayThisMonth);
-    // 今天日期格式化
-    let today = formatDate(date);
-
-    if (today === lastDayFormatted) {
-        ElNotification({
-            title: '提示',
-            message: '<span style="font-size: 20px;">今日需要进行导出抄表工作，请前往记录页面进行导出</span>',
-            type: 'warning',
-            duration: 0,
-            dangerouslyUseHTMLString: true // 允许使用 HTML 字符串
-        })
-    } else if (today === secondLastDayFormatted) {
-        ElNotification({
-            title: '提示',
-            message: '<span style="font-size: 20px;">今日需要进行录入抄表工作，请前往记录页面进行录入</span>',
-            type: 'warning',
-            duration: 0,
-            dangerouslyUseHTMLString: true // 允许使用 HTML 字符串
-        })
-    }
-}
-// 格式化日期
-function formatDate(date) {
-    // 格式化日期为 YYYY-MM-DD 的形式
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
-    return `${year}-${month}-${day}`;
-}
-
-
 const date = ref(new Date()); // 日历组件当前日期
 function isSelected (slotDate, slotData) {
     // 获取当前日期并格式化为 YYYY-MM-DD
